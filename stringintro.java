fae4b6ccb8ab46f9bb3d24ddc33a6f51f493be1b
Rohit <rohitkusv@gmail.com>
2024-09-27 23:52:08 +0530

    Create stringintro.java

---
+++ stringintro.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner input =  new Scanner(System.in);
+        
+        System.out.print("Enter the string: ");
+        String s = input.next();
+        
+        System.out.print(s);
+    }
+}
